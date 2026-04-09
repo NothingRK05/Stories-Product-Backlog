dbf32c1426158ac43871e87846f2b6b918188db9
ValdarenDev <bdog0804@gmail.com>
2026-04-09 10:37:06 -0400

    php database

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ce847db6af039cb28aef997797006032f8b8acee',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ce847db6af039cb28aef997797006032f8b8acee',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ktamas77/firebase-php' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => '80ccc4dc2996d6ebfbb2ace74569293bb94062ac',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ktamas77/firebase-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
