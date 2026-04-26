40cd427f72d412230a8def5fddd51ca3d4496552
ValdarenDev <bdog0804@gmail.com>
2026-04-26 09:47:49 -0500

    Remove php

--- database-backlog.php
+++
@@ -1,6 +0,0 @@
-const URL = 'https://stories-backlog-1a.firebaseio.com/';
-const TOKEN = 'POEsymZUmig1Cky8GPIScTxrDkYPtWVeKPEuy1Rn';
-
-use Firebase\FirebaseLib;
-
-$firebase = new FirebaseLib(URL, TOKEN);
--- database-login.php
+++
@@ -1,6 +0,0 @@
-const URL = 'https://stories-login-1a.firebaseio.com/';
-const TOKEN = 'mi4pvZLMKUk0qlNASruZ1raDgSdUqHstN6Ye1QoS';
-
-use Firebase\FirebaseLib;
-
-$firebase = new FirebaseLib(URL, TOKEN);
